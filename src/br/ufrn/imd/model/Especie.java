3f9f928ec63e6121388b928f282043e1c7905243
LucasNCortez <lucascortez6@hotmail.com>
2023-10-13 13:37:07 -0300

    Especie implementada

---
+++ src/br/ufrn/imd/model/Especie.java
@@ -0,0 +1,8 @@
+package br.ufrn.imd.model;
+
+public class Especie {
+  public static final String CAO = "Cão";
+  public static final String GATO = "Gato";
+  public static final String COELHO = "Coelho";
+  public static final String RATO = "Rato";
+}
