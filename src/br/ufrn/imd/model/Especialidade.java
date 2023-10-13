a9280c0a07866df6eaaffb7b72393c155b9bcdb6
LucasNCortez <lucascortez6@hotmail.com>
2023-10-13 02:34:33 -0300

    Especialidade implementada

--- src/br/ufrn/imd/model/Especialidade.java
+++ src/br/ufrn/imd/model/Especialidade.java
@@ -1,5 +1,21 @@
 package br.ufrn.imd.model;
 
 public class Especialidade {
-    
+  private int id;
+  private String nome;
+
+  public int getId() {
+    return this.id;
+  }
+  public void setId(int newId){
+    this.id = newId;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String newNome) {
+    this.nome = newNome;
+  }
 }
